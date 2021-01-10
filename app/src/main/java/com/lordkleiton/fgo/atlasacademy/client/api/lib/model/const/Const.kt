bc9155e55913bd3984b706c624845ae74889d156
lord <m_s_r@outlook.com>
2021-01-10 00:58:47 -0300

    adicionando fallback pra bool

--- app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/api/lib/model/const/Const.kt
+++ app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/api/lib/model/const/Const.kt
@@ -3,4 +3,5 @@ package com.lordkleiton.fgo.atlasacademy.client.api.lib.model.const
 object Const {
     const val FALLBACK_INT = Int.MAX_VALUE
     const val FALLBACK_STRING = "FALLBACK STRING"
+    val FALLBACK_BOOL : Boolean? = null
 }
--- app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/api/lib/model/nice/NiceTrait.kt
+++ app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/api/lib/model/nice/NiceTrait.kt
@@ -1,10 +1,11 @@
 package com.lordkleiton.fgo.atlasacademy.client.api.lib.model.nice
 
+import com.lordkleiton.fgo.atlasacademy.client.api.lib.model.const.Const.FALLBACK_BOOL
 import com.lordkleiton.fgo.atlasacademy.client.api.lib.model.enum.TraitEnum
 import com.lordkleiton.fgo.atlasacademy.client.api.lib.model.util.findEnumByName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class NiceTrait(val id: Int, val negative: Boolean = false, val name: String) {
-    val traitName = findEnumByName<TraitEnum>(name)
+data class NiceTrait(val id: Int, val negative: Boolean? = FALLBACK_BOOL, val name: String) {
+    val nameEnum = findEnumByName<TraitEnum>(name)
 }
