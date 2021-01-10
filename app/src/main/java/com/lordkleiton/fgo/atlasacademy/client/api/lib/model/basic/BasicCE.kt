5cbd73be9ddbfa8def25f4a60f5fdcc1100639bb
lord <m_s_r@outlook.com>
2021-01-09 23:28:49 -0300

    marcando essa galera como serializable

--- app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/api/lib/model/basic/BasicCE.kt
+++ app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/api/lib/model/basic/BasicCE.kt
@@ -2,7 +2,9 @@ package com.lordkleiton.fgo.atlasacademy.client.api.lib.model.basic
 
 import com.lordkleiton.fgo.atlasacademy.client.api.lib.model.const.Const.FALLBACK_INT
 import com.lordkleiton.fgo.atlasacademy.client.api.lib.model.const.Const.FALLBACK_STRING
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class BasicCE(
     val id: Int = FALLBACK_INT,
     val collectionNo: Int = FALLBACK_INT,
