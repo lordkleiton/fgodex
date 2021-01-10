0b128759a2aaf28946fc5a14be3889db1c714f90
lord <m_s_r@outlook.com>
2021-01-09 23:26:18 -0300

    constantes de fallback da lib da api

---
+++ app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/api/lib/model/const/Const.kt
@@ -0,0 +1,6 @@
+package com.lordkleiton.fgo.atlasacademy.client.api.lib.model.const
+
+object Const {
+    const val FALLBACK_INT = Int.MAX_VALUE
+    const val FALLBACK_STRING = "FALLBACK STRING"
+}
