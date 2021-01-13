2e04c44c36e064f41f882d1849117ac5325d0985
Mario <mario@mobills.com.br>
2021-01-13 15:44:39 -0300

    facilitando a vida com um botão

--- app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/MainActivity.kt
+++ app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/MainActivity.kt
@@ -1,26 +1,31 @@
 package com.lordkleiton.fgo.atlasacademy.client
 
 import android.os.Bundle
-import android.os.Looper
-import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.lordkleiton.fgo.atlasacademy.client.api.lib.model.basic.BasicServant
-import com.lordkleiton.fgo.atlasacademy.client.api.lib.model.nice.NiceServant
 import com.lordkleiton.fgo.atlasacademy.client.api.lib.request.ApiRequestHandler
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch(Dispatchers.Main) {
-            val result = withContext(Dispatchers.Default) {
-                ApiRequestHandler.get<BasicServant>()
-            }
+        val btn = findViewById<Button>(R.id.btn)
 
-            Toast.makeText(baseContext, result.toString(), Toast.LENGTH_LONG).show()
+        btn.setOnClickListener {
+            GlobalScope.launch(Dispatchers.Main) {
+                val result = withContext(Dispatchers.Default) {
+                    ApiRequestHandler.get<BasicServant>()
+                }
+
+                Toast.makeText(baseContext, result.toString(), Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
