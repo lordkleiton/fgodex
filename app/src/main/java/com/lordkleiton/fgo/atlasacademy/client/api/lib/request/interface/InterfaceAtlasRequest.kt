1f2650bbae0ac250d37791f399149905ee48178b
lord <m_s_r@outlook.com>
2021-01-14 00:00:49 -0300

    removendo coisas hardcoded por enums

--- app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/api/lib/request/interface/InterfaceAtlasRequest.kt
+++ app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/api/lib/request/interface/InterfaceAtlasRequest.kt
@@ -1,23 +1,31 @@
 package com.lordkleiton.fgo.atlasacademy.client.api.lib.request.`interface`
 
+import com.lordkleiton.fgo.atlasacademy.client.api.lib.request.const.Const.FIND_URL
+import com.lordkleiton.fgo.atlasacademy.client.api.lib.request.const.Const.GET_URL
+import com.lordkleiton.fgo.atlasacademy.client.api.lib.request.const.Const.URL_ENDPOINT
+import com.lordkleiton.fgo.atlasacademy.client.api.lib.request.const.Const.URL_ID
+import com.lordkleiton.fgo.atlasacademy.client.api.lib.request.const.Const.URL_REGION
+import com.lordkleiton.fgo.atlasacademy.client.api.lib.request.const.Const.URL_TYPE
 import kotlinx.serialization.json.JsonElement
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.QueryMap
 
 interface InterfaceAtlasRequest {
-    @GET("{type}/{region}/{endpoint}/search")
-    fun find(
-        @Path("endpoint") endpoint: String,
-        @Path("region") region: String = "NA",
-        @Path("type") type: String = "basic"
+    @GET(GET_URL)
+    fun get(
+        @Path(URL_TYPE) type: String,
+        @Path(URL_REGION) region: String,
+        @Path(URL_ENDPOINT) endpoint: String,
+        @Path(URL_ID) id: String,
     ): Call<JsonElement>
 
-    @GET("{type}/{region}/{endpoint}/{id}")
-    fun get(
-        @Path("endpoint") endpoint: String,
-        @Path("id") id: String,
-        @Path("region") region: String = "NA",
-        @Path("type") type: String = "basic",
+    @GET(FIND_URL)
+    fun find(
+        @Path(URL_TYPE) type: String,
+        @Path(URL_REGION) region: String,
+        @Path(URL_ENDPOINT) endpoint: String,
+        @QueryMap options: Map<String, String>,
     ): Call<JsonElement>
 }
