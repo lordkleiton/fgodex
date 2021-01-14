629cdb9c0546578ed0d3e6571e55e8f6f4d22fd2
lord <m_s_r@outlook.com>
2021-01-13 23:59:58 -0300

    novas constantes pra requisições

---
+++ app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/api/lib/request/const/Const.kt
@@ -0,0 +1,12 @@
+package com.lordkleiton.fgo.atlasacademy.client.api.lib.request.const
+
+object Const {
+    const val MEDIA_TYPE = "application/json"
+    const val BASE_URL = "https://api.atlasacademy.io/"
+    const val URL_TYPE = "type"
+    const val URL_REGION = "region"
+    const val URL_ENDPOINT = "endpoint"
+    const val URL_ID = "id"
+    const val FIND_URL = "{$URL_TYPE}/{$URL_REGION}/{$URL_ENDPOINT}/search"
+    const val GET_URL = "{$URL_TYPE}/{$URL_REGION}/{$URL_ENDPOINT}/{$URL_ID}"
+}
