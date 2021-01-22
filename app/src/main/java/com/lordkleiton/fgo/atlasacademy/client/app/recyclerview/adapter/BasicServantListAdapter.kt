98d62a1d4368c3d65e949884c9397ec22f085123
lord <m_s_r@outlook.com>
2021-01-22 02:08:56 -0300

    dando cor de background baseado na raridade

--- app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/app/recyclerview/adapter/BasicServantListAdapter.kt
+++ app/src/main/java/com/lordkleiton/fgo/atlasacademy/client/app/recyclerview/adapter/BasicServantListAdapter.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
@@ -73,11 +75,13 @@ class BasicServantListAdapter(
         private lateinit var servant: BasicServant
         private val img: ImageView
         private val svtId: TextView
+        private val svtBgCard: CardView
 
         init {
             view.apply {
                 img = findViewById(R.id.list_item_servant_img)
                 svtId = findViewById(R.id.list_item_servant_id)
+                svtBgCard = findViewById(R.id.list_item_servant_background_card)
 
                 setOnClickListener {
                     clickListener.onItemClick(servant, adapterPosition)
@@ -89,6 +93,18 @@ class BasicServantListAdapter(
             this.servant = servant
 
             svtId.text = servant.collectionNo.toString()
+
+            svtBgCard.apply {
+                val color = when (servant.rarity) {
+                    0 -> R.color.black
+                    1 -> R.color.bronze
+                    2 -> R.color.bronze
+                    3 -> R.color.silver
+                    else -> R.color.gold
+                }
+
+                background.setTint(ContextCompat.getColor(context, color))
+            }
 
             Glide.with(context).load(servant.face).placeholder(R.drawable.ic_launcher_foreground)
                 .transform(
